298d70de40b55d0e96ce47c2c835c03819d95f62
Naveed <mnrnavi@gmail.com>
2025-04-13 17:08:09 +0500

    Calculator Task

---
+++ main.dart
@@ -0,0 +1,23 @@
+void main () {
+ //   print "Pakistan Zindabad"
+ //   print "Pakistan Paindabad"
+ // int num1 = 10
+ // int num2 = 10
+
+ // print(email);
+ print("Login Calculator");
+
+ // string? stdName = stdin.readLineSync();
+ // int num19 = intparse(stdin.readLineSync()?? "0");
+
+ // if (num1 != num2)
+    String email = "naveedriaz@gmail.com";
+    String pasword = "mnr012";
+
+    print(email);
+    print(password);
+
+    String Your Email =  stdin.readLineSync();
+    
+
+}
